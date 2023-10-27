870eb8a350cb9152a7d382cea6c8b0f19a14f772
Emily Gavrilenko <egavrile@calpoly.edu>
2023-10-27 09:20:35 -0700

    Update endpoint URL

--- src/api/inference.js
+++ src/api/inference.js
@@ -3,10 +3,11 @@ import axios from "axios";
 export async function getMusicNoteInference(image) {
     return await axios({
         method: "POST",
-        url: "https://detect.roboflow.com/sheet-music-player/5",
+        url: "https://detect.roboflow.com/sheet-music-player/9",
         params: {
             api_key: process.env.REACT_APP_ROBOFLOW_API_KEY,
-            confidence: 20
+            confidence: 20,
+            overlap: 30
         },
         data: image,
         headers: {
